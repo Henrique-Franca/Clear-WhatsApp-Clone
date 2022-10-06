f31b3d976f94647ce56b5a31db9a85fdb8f9052c
HenriqueFranca <henriqsf@gmail.com>
2022-10-06 10:37:10 -0300

    Prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         console.log('WhatsAppController ok');
 
+        this.elementPrototype();
         this.loadElements();
 
 
@@ -18,6 +19,72 @@ class WhatsAppController {
             this.el[Format.getCamelCase(element.id)] = element;
 
         });
+
+    }
+
+    elementPrototype(){
+
+        Element.prototype.hide = function(){
+
+            this.style.display = 'none';
+
+        }
+
+        Element.prototype.show = function(){
+
+            this.style.display = 'block';
+
+        }
+
+        Element.prototype.toggle = function(){
+
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+
+        }
+
+        Element.prototype.on = function(){
+
+            events.split(' ').forEach(event=>{
+
+                this.addEventListener(event, fn);
+
+            });
+
+        }
+
+        Element.prototype.css =  function(styles){
+
+            for(let name in styles){
+
+                this.style[name] = styles[name];
+
+            }
+
+        }
+
+        Element.prototype.addClass = function(name){
+
+            this.classList.add(name);
+
+        }
+
+        Element.prototype.removeClass = function(name){
+
+            this.classList.remove(name);
+
+        }
+
+        Element.prototype.toggleClass = function(name){
+
+            this.classList.toggle(name);
+
+        }
+
+        Element.prototype.hasClass = function(name){
+
+            return this.classList.contains(name);
+
+        }
 
     }
 
