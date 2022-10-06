7529f03cf922e456070792b7087ac6daec34b062
HenriqueFranca <henriqsf@gmail.com>
2022-10-06 11:30:37 -0300

    Eventos para abrir/fechar os painéis Editar Perfil e Adicionar Contato

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ class WhatsAppController {
 
         this.elementPrototype();
         this.loadElements();
+        this.initEvents();
 
 
     }
@@ -42,13 +43,14 @@ class WhatsAppController {
 
         }
 
-        Element.prototype.on = function(){
+        Element.prototype.on = function(events,fn){
 
             events.split(' ').forEach(event=>{
 
                 this.addEventListener(event, fn);
 
             });
+            return this;
 
         }
 
@@ -88,6 +90,50 @@ class WhatsAppController {
 
     }
 
+    initEvents(){
 
+        this.el.myPhoto.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+
+        });  
+
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+
+        });  
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+
+        });  
+
+        this.el.btnClosePanelAddContact.on('click', e=>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        });  
+
+    }
+
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+    }
 
 }
