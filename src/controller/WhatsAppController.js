ef83002decf731988b6dd0fcdce9af7e2d80d66d
HenriqueFranca <henriqsf@gmail.com>
2022-10-11 09:50:42 -0300

    Lendo capa de arquivo PDF e gerando imagem com PDF.js

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -284,6 +284,10 @@ export class WhatsAppController {
 
             if(this.el.inputDocument.files.length){
 
+                this.el.panelDocumentPreview.css({
+                    'height':'1%'
+                });
+
                 let file = this.el.inputDocument.files[0];
 
                 this._documentPreviewController = new DocumentPreviewController(file);
@@ -295,9 +299,17 @@ export class WhatsAppController {
                     this.el.imagePanelDocumentPreview.show();
                     this.el.filePanelDocumentPreview.hide();
 
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 120px)'
+                    });
+
                 }).catch(err=>{
 
-                    console.log(file.type);
+                    this.el.panelDocumentPreview.css({
+                        'height':'calc(100% - 120px)'
+                    });
+
+                    //console.log(file.type);
 
                     switch(file) {
 
