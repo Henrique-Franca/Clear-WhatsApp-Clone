569c4a6f50a97139a732420b54f9741ee8f73f57
HenriqueFranca <henriqsf@gmail.com>
2022-10-17 12:44:59 -0300

    Lendo as mensagens de um chat em Tempo Real (Realtime)

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,4 +1,5 @@
 import { Firebase } from "../util/Firebase";
+import { Format } from "../util/Format";
 import { Model } from "./Model";
 
 export class Menssage extends Model{
@@ -8,6 +9,9 @@ export class Menssage extends Model{
         super();
 
     }
+
+    get id(){ return this._data.id; }
+    set id(value){ return this._data.id = value; }
 
     get content(){ return this._data.content; }
     set content(value){ return this._data.content = value; }
@@ -271,16 +275,16 @@ export class Menssage extends Model{
 
             default:
                 div.innerHTML = `
-                <div class="font-style _3DFk6 tail">
+                <div class="font-style _3DFk6 tail" id="_${this.id}">
                     <span class="tail-container"></span>
                     <span class="tail-container highlight"></span>
                     <div class="Tkt2p">
                         <div class="_3zb-j ZhF0n">
-                            <span dir="ltr" class="selectable-text invisible-space message-text">Oi!</span>
+                            <span dir="ltr" class="selectable-text invisible-space message-text">${this.content}</span>
                         </div>
                         <div class="_2f-RV">
                             <div class="_1DZAH">
-                                <span class="msg-time">11:33</span>
+                                <span class="msg-time">${Format.timeStampTotime(this.timeStamp)}</span>
                             </div>
                         </div>
                     </div>
